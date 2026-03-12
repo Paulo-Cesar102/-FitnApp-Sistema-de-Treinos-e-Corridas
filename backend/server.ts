8f400f995703f99f4509f0e2d95a1d99cf295747
Paulo-Cesar102 <153522516+Paulo-Cesar102@users.noreply.github.com>
2026-03-12 11:17:11 -0300

    atualização de bugs

--- backend/server.ts
+++ backend/server.ts
@@ -8,22 +8,21 @@ import { workoutRoutes } from "./src/routes/workout.routes";
 
 const app = express();
 
-// Middlewares Globais
-app.use(cors()); // Libera o acesso para o seu front e o do seu amigo
-app.use(express.json()); // Faz o Express entender JSON no corpo da requisição (body)
+app.use(cors()); 
+app.use(express.json()); 
 
-// Definição das Rotas (Organização por recurso)
+
 app.use("/api", exerciseRoutes);
 app.use("/users", userRoutes);
 app.use("/feedbacks", feedbackRoutes);
 app.use("/workouts", workoutRoutes);
 app.use("/auth", authRoutes);
 
-// Rota de Health Check (Verificar se o server está vivo)
+
 app.get('/', (_req, res) => {
   res.json({ 
     status: 'online',
-    message: 'Server deAcademia rodando a todo vapor! 🚀' 
+    message: 'Server conectado com sucesso!' 
   });
 });
 
