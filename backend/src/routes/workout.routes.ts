d6b799e41d64835f35e523b97447681f0f2b0df0
Paulo-Cesar102 <153522516+Paulo-Cesar102@users.noreply.github.com>
2026-03-12 11:34:21 -0300

    atualização de bugs

--- backend/src/routes/workout.routes.ts
+++ backend/src/routes/workout.routes.ts
@@ -4,8 +4,8 @@ import { WorkoutController } from "../controller/WorkoutController";
 const workoutRoutes = Router();
 const workoutController = new WorkoutController();
 
-workoutRoutes.post("/", (req, res) => workoutController.create(req, res));
-workoutRoutes.get("/", (req, res) => workoutController.findAll(req, res));
+workoutRoutes.post("/create", (req, res) => workoutController.create(req, res));
+workoutRoutes.get("/list", (req, res) => workoutController.findAll(req, res));
 workoutRoutes.delete("/:id", (req, res) => workoutController.delete(req, res));
 
 export { workoutRoutes };
